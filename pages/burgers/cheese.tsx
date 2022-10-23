a223c33abd4bdd4cf026d7c7dfbc0fc7e9695e52
vmeset <vmeset@gmail.com>
2022-10-23 15:41:20 +0300

    project created

---
+++ pages/burgers/cheese.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Cheese: NextPage = () => {
+    return (
+        <div>
+            ChesseBurger
+        </div>
+    );
+}
+
+export default Cheese
---
+++ pages/burgers/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import styles from '../../styles/Home.module.css'
+
+const Burgers: NextPage = () => {
+    return (
+        <div>
+            all burgerZ
+        </div>
+    )
+}
+
+export default Burgers
